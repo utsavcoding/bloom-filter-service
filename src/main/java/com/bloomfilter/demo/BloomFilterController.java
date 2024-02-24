4d8e1693b000696a64fed954eaf7222178d6799d
utsavcoding <utsavtiwary10@gmail.com>
2024-02-25 02:28:26 +0530

    cid-1: Initial commit with bloom rest controller

---
+++ src/main/java/com/bloomfilter/demo/BloomFilterController.java
@@ -0,0 +1,14 @@
+package com.bloomfilter.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BloomFilterController {
+
+    @GetMapping("/bloom")
+    public String bloom(@RequestParam(value = "name",defaultValue = "World") String name){
+        return String.format("Bloom "+name);
+    }
+}
